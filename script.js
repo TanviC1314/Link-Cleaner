3ef0769abe297c3a3a59ca86ff8408f553067e31
TanviC1314 <tanvivchiman.com>
2026-01-08 21:31:02 +0000

    toUrlObj

--- script.js
+++ script.js
@@ -76,6 +76,22 @@ function toUrlObject(text) {
   // 1. Try new URL(text)
   // 2. If success → return URL object
   // 3. If error → return null
+  //https://example.com/page?x=1#top
+  let url;
+  try{
+    url =new URL(text);
+    const obj={
+      host: url.hostname,
+      path:url.pathname,
+      search:url.search,
+      hash:url.hash
+    }
+    return obj
+  }catch(_){
+    return null
+  }
+
+  console.log(url.hostname)
 }
 
 
